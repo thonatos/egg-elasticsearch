710c125dc786f8e2ab22c7a1c8ac573696f8e749
焰寒 <yanhan.zxf@alibaba-inc.com>
2019-01-30 20:19:15 +0800

    chore: revert unittest host setting

--- test/fixtures/apps/elasticsearch-test/config/config.unittest.js
+++ test/fixtures/apps/elasticsearch-test/config/config.unittest.js
@@ -3,6 +3,6 @@
 exports.keys = '123456';
 
 exports.elasticsearch = {
-  host: 'http://100.84.73.159:9200',
-  httpAuth: 'elastic:changeme',
+  host: 'localhost:9200',
+  // httpAuth: '', // user:pass
 };
